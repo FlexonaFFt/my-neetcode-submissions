e7c1cf9d4fc70da3de88e373847efbe24f3856cf
Жаров Игорь <66370684+FlexonaFFt@users.noreply.github.com>
2026-05-15 13:09:29 +0300

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.py
@@ -0,0 +1,18 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        curr_span = 1
+        while self.stack and self.stack[-1][0] <= price:
+
+            curr_span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append((price, curr_span))
+        return curr_span
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
